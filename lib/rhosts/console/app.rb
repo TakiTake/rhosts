20e1db1280f0de2d78c1cccb44aa4d4464610a39
Takizawa Takeshi <takitake.create@gmail.com>
2013-06-11 06:27:04 +0900

    add initialize

--- lib/rhosts/console/app.rb
+++ lib/rhosts/console/app.rb
@@ -1,25 +1,26 @@
 module RHosts
   module ConsoleMethods
-    def map(target)
-      @actives ||= {}
+    def initialize
+      @actives   = {}
+      @inactives = {}
+    end
 
-      aaa(target) do |host, ip|
+    def map(target)
+      process(target) do |host, ip|
         @actives[ip] ||= []
         @actives[ip] << host
       end
     end
 
     def unmap(target)
-      @inactives ||= {}
-
-      aaa(target) do |host, ip|
+      process(target) do |host, ip|
         @inactives[ip] ||= []
         @inactives[ip] << host
       end
     end
 
     private
-    def aaa(target, &block)
+    def process(target, &block)
       raise ArgumentsError.new('mapping target must be Hash') unless target.instance_of? Hash
 
       # TODO
@@ -27,8 +28,6 @@ module RHosts
 
       target.each do |host, ip|
         block.call(host, ip)
-        p @actives
-        p @inactives
       end
 
       # TODO
