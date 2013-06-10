62b408a393d32980c0cac8ec41d0bd9b33d795f6
Takizawa Takeshi <takitake.create@gmail.com>
2013-06-11 06:28:38 +0900

    add spec

--- Gemfile
+++ Gemfile
@@ -1,3 +1,12 @@
 source "https://rubygems.org"
 
 gemspec
+
+group :development do
+  gem 'awesome_print', '>= 1.1.0'
+  gem 'pry-debugger', '>= 0.2.2'
+end
+
+group :test do
+  gem 'rspec', '>= 2.13.0'
+end
---
+++ spec/console/app_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+require 'rhosts/console/app'
+
+describe 'ConsoleMethods' do
+  include RHosts::ConsoleMethods
+
+  describe '#map' do
+    before do
+      map 'example.com' => '127.0.0.1'
+    end
+
+    after do
+      @actives.clear
+      @inactives.clear
+    end
+
+    it 'map example.com to 127.0.0.1' do
+      expect(@actives).to eq('127.0.0.1' => ['example.com'])
+      expect(@inactives).to eq({ })
+    end
+  end
+end
