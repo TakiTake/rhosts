fa3b51c73343549ea853a77efa739b3e42e4fa31
Takeshi Takizawa <takeshi.takizawa@mail.rakuten.com>
2013-06-11 10:14:49 +0900

    use readline instead of irb enable use free word

--- lib/rhosts/cli.rb
+++ lib/rhosts/cli.rb
@@ -1,5 +1,7 @@
 Signal.trap("INT") { puts; exit(1) }
 
+require 'rhosts'
+
 # TODO: Runnable not only console but also exec from file, string and so on.
 require 'rhosts/commands/console'
 
--- lib/rhosts/commands/console.rb
+++ lib/rhosts/commands/console.rb
@@ -1,17 +1,30 @@
 require 'pp'
+require 'readline'
 require 'rhosts/console/app'
-require 'irb'
-require 'irb/completion'
 
 module RHosts
   class Console
+    include RHosts::ConsoleMethods
+
     def self.start
       new.start
     end
 
     def start
-      IRB::ExtendCommandBundle.send :include, RHosts::ConsoleMethods
-      IRB.start
+      while cmd = Readline.readline('rhosts> ', true)
+        case cmd.chomp
+        when 'a', 'actives'
+          p actives
+        when 'i', 'inactives'
+          p actives
+        when 'history'
+          p Readline::HISTORY.to_a
+        when 'h', 'help'
+          help
+        when 'q', 'quit'
+          exit
+        end
+      end
     end
   end
 end
